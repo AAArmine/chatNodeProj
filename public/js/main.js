284b10bcbe3ab1d263c062ca5aed600647fc9b68
AAArmine <aaarmine.aghababyan@gmail.com>
2024-03-28 01:17:50 +0400

    user leaving room and joining

--- public/js/main.js
+++ public/js/main.js
@@ -60,7 +60,6 @@ function outputUsers(users) {
     userList.appendChild(li);
   });
 }
-
 //Prompt the user before leave chat room
 document.getElementById('leave-btn').addEventListener('click', () => {
   const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
